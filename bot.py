a88f8f92e8aa2de9aed6a9345d4bbba23ca947df
Vinicius Soares <vsdc.snf19@uea.edu.br>
2021-05-27 00:28:58 -0400

    adicionado sistema básico de processamento de pergunta

---
+++ bot.py
@@ -0,0 +1,33 @@
+import regex as re
+from database import database
+
+def GenerateAnswer(question):
+  keyword = GetKeyWord(question)
+  try:
+    answer = database[keyword]
+  except:
+    return database['default'] # se não houver uma resposta pronta, retorna uma resposta padrão, ex: "não entendi"
+  else:
+    return answer
+
+# recebe a pergunta(str) e retorna uma lista de palavras-chave encontradas na pergunta
+def GetKeyWord(question):
+  question = question.lower()
+  keyword = ''
+  keylist = database.keys()
+  for key in keylist:
+    if match := re.search(key, question):
+      keyword = key
+  return keyword
+  
+
+
+#debug
+def main():
+  question = input()
+  answer = GenerateAnswer(question)
+  print(answer)
+
+if __name__ == "__main__":
+  main()
+#debug
