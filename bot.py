dbd4fad73dab62ff50f855954493d4acaa85d81f
YMMO18 <ymmdo.snf19@uea.edu.br>
2021-05-28 11:00:16 -0400

    adicionando pagamento, endereço e confirmação do pedido

--- bot.py
+++ bot.py
@@ -1,14 +1,18 @@
 import regex as re
-from database import database
+from database import database, menu
+
 
 def GenerateAnswer(question):
   keyword = GetKeyWord(question)
+  order = ConfirmOrder(keyword, question)
   try:
     answer = database[keyword]
+    answer += order
   except:
     return database['default'] # se não houver uma resposta pronta, retorna uma resposta padrão, ex: "não entendi"
   else:
     return answer
+
 
 # recebe a pergunta(str) e retorna uma lista de palavras-chave encontradas na pergunta
 def GetKeyWord(question):
@@ -20,14 +24,35 @@ def GetKeyWord(question):
     if match := re.search(key, question):
       keyword = key
   return keyword
-  
+
+
+register = {'cart':'', 'total': 0, 'pay': '', 'address': ''}
+
+def ConfirmOrder(keyword, question):
+  if keyword in menu.keys():
+    register['cart'] += f'{keyword} '
+    register['total'] += menu[keyword]
+  elif keyword in ['cartao', 'dinheiro']:
+    register['pay'] += question
+  elif keyword == 'rua':
+    register['address'] += question
+    order = f"  Carrinho: {register['cart']}\n  Total: {register['total']} reais\n" +\
+			      f"  Forma de pagamento: {register['pay']}\n" +\
+			      f"  Endereço de entrega: {register['address']}"
+    register['cart'] = register['pay'] = register['address'] = ''
+    register['total'] = 0
+    return order
+  return ''
+
 
 
 #debug
 def main():
-  question = input()
-  answer = GenerateAnswer(question)
-  print(answer)
+  question = ''
+  while question != 'fim':
+    question = input()
+    answer = GenerateAnswer(question)
+    print(answer)
 
 if __name__ == "__main__":
   main()
--- database.py
+++ database.py
@@ -1,23 +1,43 @@
 from random import choice
 
-cumprimentos = ['> Olá! Gostaria de ver nosso cardápio?',
-                '> Oi! Que tal dar uma olhada no nosso cardápio?']
+greeting = ['> Olá! Gostaria de ver nosso cardápio?',
+            '> Oi! Que tal dar uma olhada no nosso cardápio?']
 
-cardapios = [['x-salada', 'suco', 'combo'],
-            ['pastel', 'refrigerante', 'fritas'],
-            ['sushi', 'sashimi', 'yakisoba'],
-            ['sorvete', 'chocolate quente', 'pudim']]
+farewell = ['> Obrigado pela preferência! Volte sempre! :D',
+            '> Teve uma boa experiência? Volte sempre! :)',
+            '> Até mais! Volte sempre! :3']
 
-atual = choice(cardapios)
-#print(atual)
+menu = {'x-salada': 6, 'suco': 3.5, 'pastel': 5,
+        'refrigerante': 4.5, 'fritas': 4, 'pudim': 30}
 
-database = {'oi': f'{choice(cumprimentos)}',
-            'ola': f'{choice(cumprimentos)}',
-            'olhar': f"> Este é o cardápio de hoje:\n  {' | '.join(atual)}",
-            'ver': f"> Este é o cardápio de hoje:\n  {' | '.join(atual)}",
-            'cardapio': f"> Este é o cardápio de atual:\n  {' | '.join(atual)}",
-            atual[0]: f"> O item {atual[0]} foi adicionado ao seu carrinho!",
-            atual[1]: f"> O item {atual[1]} foi adicionado ao seu carrinho!",
-            atual[2]: f"> O item {atual[2]} foi adicionado ao seu carrinho!",
-            'tchau': f'Até mais! Volte sempre! :D',
-            'default':'Sinto muito. Não entendi. :('}
+database = {'oi': choice(greeting),
+            'ola': choice(greeting),
+            
+            'olhar': '> Olha só o maravilhoso cardápio de hoje:\n  '+'\n  '.join(menu.keys()),
+            'ver': '> Olha só o maravilhoso cardápio de hoje:\n  '+'\n  '.join(menu.keys()),
+            'cardapio': '> Olha só o maravilhoso cardápio de hoje:\n  '+'\n  '.join(menu.keys()),
+            
+            'x-salada': '> O item x-salada foi adicionado ao seu carrinho!',
+            'suco': '> O item suco foi adicionado ao seu carrinho!',
+            'pastel': '> O item pastel foi adicionado ao seu carrinho!',
+            'refrigerante': '> O item refrigerante foi adicionado ao seu carrinho!',
+            'fritas': '> O item fritas foi adicionado ao seu carrinho!',
+            'pudim': '> O item pudim foi adicionado ao seu carrinho!',
+
+            'apenas': '> Carrinho preenchido! Como você gostaria de efetuar o pagamento?',
+            'somente': '> Carrinho finalizado! Qual será a forma de pagamento?',
+
+            'dinheiro': '> Certo! Prepare suas cédulas para o entregador! Onde ele deve entregar?',
+            'cartao': '> Ok! Levaremos a maquininha para você! Qual será o endereço da entrega?',
+
+            'rua': '> Tudo certo! Endereço definido! Deseja confirmar ou descartar o pedido a seguir?\n',
+
+			'confirmar': '> Pedido confirmado com sucesso. Aguarde a entrega. Até a próxima!',
+			'descartar': '> Pedido descartado. Que tal recomeçar? Deseja ver o cardápio de novo?',
+
+            'tchau': choice(farewell),
+            'adeus': choice(farewell),
+            'encerrar': choice(farewell),
+            'fim': choice(farewell),
+            
+            'default':'> Sinto muito. Não entendi. :('}
