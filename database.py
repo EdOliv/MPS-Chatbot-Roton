396b65edcee868c29c6095560ddc2775db120b63
Vinicius Soares <vsdc.snf19@uea.edu.br>
2021-05-28 11:41:37 -0400

    Ajustado formatação das palavras-chave

--- database.py
+++ database.py
@@ -7,15 +7,15 @@ farewell = ['> Obrigado pela preferência! Volte sempre! :D',
             '> Teve uma boa experiência? Volte sempre! :)',
             '> Até mais! Volte sempre! :3']
 
+endOrder = ['> Carrinho preenchido! Como você gostaria de efetuar o pagamento?',
+            '> Carrinho finalizado! Qual será a forma de pagamento?']
+
 menu = {'x-salada': 6, 'suco': 3.5, 'pastel': 5,
         'refrigerante': 4.5, 'fritas': 4, 'pudim': 30}
 
-database = {'oi': choice(greeting),
-            'ola': choice(greeting),
+database = {'oi|ola': choice(greeting),
             
-            'olhar': '> Olha só o maravilhoso cardápio de hoje:\n  '+'\n  '.join(menu.keys()),
-            'ver': '> Olha só o maravilhoso cardápio de hoje:\n  '+'\n  '.join(menu.keys()),
-            'cardapio': '> Olha só o maravilhoso cardápio de hoje:\n  '+'\n  '.join(menu.keys()),
+            'olhar|ver|cardapio': '> Olha só o maravilhoso cardápio de hoje:\n  '+'\n  '.join(menu.keys()),
             
             'x-salada': '> O item x-salada foi adicionado ao seu carrinho!',
             'suco': '> O item suco foi adicionado ao seu carrinho!',
@@ -24,8 +24,7 @@ database = {'oi': choice(greeting),
             'fritas': '> O item fritas foi adicionado ao seu carrinho!',
             'pudim': '> O item pudim foi adicionado ao seu carrinho!',
 
-            'apenas': '> Carrinho preenchido! Como você gostaria de efetuar o pagamento?',
-            'somente': '> Carrinho finalizado! Qual será a forma de pagamento?',
+            'apenas|somente' : choice(endOrder),
 
             'dinheiro': '> Certo! Prepare suas cédulas para o entregador! Onde ele deve entregar?',
             'cartao': '> Ok! Levaremos a maquininha para você! Qual será o endereço da entrega?',
@@ -35,9 +34,6 @@ database = {'oi': choice(greeting),
 			'confirmar': '> Pedido confirmado com sucesso. Aguarde a entrega. Até a próxima!',
 			'descartar': '> Pedido descartado. Que tal recomeçar? Deseja ver o cardápio de novo?',
 
-            'tchau': choice(farewell),
-            'adeus': choice(farewell),
-            'encerrar': choice(farewell),
-            'fim': choice(farewell),
+            'tchau|adeus|encerrar|fim': choice(farewell),
             
             'default':'> Sinto muito. Não entendi. :('}
